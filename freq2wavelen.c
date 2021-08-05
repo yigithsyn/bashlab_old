07ed8768079f9742d9fe0eeaa44ec110ef8bfef4
Hüseyin <iletisim@huseyinyigit.com.tr>
2021-08-05 09:11:32 +0300

    debug routines added

--- freq2wavelen.c
+++ freq2wavelen.c
@@ -1,13 +1,17 @@
 #define HELP "\n"                                                        \
              "Frequency to wavelength convert\n"                         \
-             "Usage: freq2wavelen <freq(s)> \n"                             \
-             "                    <freq(s)> <out> \n\n"                     \
+             "Usage: freq2wavelen <freq(s)> \n"                          \
+             "                    <freq(s)> <out> \n\n"                  \
              "<freq>: input value [double] or variable name [string] \n" \
              "<out> : output variable name [string] \n"
 
+#define DEBUG 1
+
+#include "macros.h"
+
 #include <stdio.h>
+#include <string.h>
 // #include <ctype.h>
-// #include <string.h>
 // #include <stdlib.h>
 #include <sys/stat.h>
 #include <stdbool.h>
@@ -22,8 +26,10 @@ bool file_exists(const char *filename)
 
 #include "jansson.h"
 
-#define C0 299792458        // Speed of light in m/s
-// #define MAX_FREQ_STR_LEN 50 //
+static struct stat stat_buff;
+static json_error_t *json_error;
+static json_t *workspace;
+static bool valid_workspace = false;
 
 int main(int argc, char *argv[])
 {
@@ -34,70 +40,104 @@ int main(int argc, char *argv[])
     return 0;
   }
 
-  struct stat stat_buff;
-  double arg = 0, *args = NULL;
-  double val = 0, *vals = NULL;
+  double *freq = malloc(sizeof(double)), *wlen = malloc(sizeof(double));
+  workspace = json_load_file(WORKSPACE, 0, json_error);
+  if (stat(WORKSPACE, &stat_buff) == 0)
+    if (workspace != NULL && json_typeof(workspace) == JSON_OBJECT)
+      valid_workspace = true;
+
+  if (DEBUG)
+    if (valid_workspace)
+      printf("[DEBUG] Workspace file found.\n");
+    else
+      printf("[DEBUG] Workspace file not found.\n");
 
   if (argc >= 2)
   {
-    bool var_found = false;
-    if (stat("workspace.json", &stat_buff) == 0)
+    if (valid_workspace)
     {
-      json_error_t *json_error = NULL;
-      json_t *workspace = json_load_file("workspace.json", 0, json_error);
-      if (workspace != NULL && json_typeof(workspace) == JSON_OBJECT)
+      size_t index;
+      json_t *variable;
+      json_array_foreach(json_object_get(workspace, "variables"), index, variable)
       {
-        size_t index;
-        json_t *value;
-        json_array_foreach(json_object_get(workspace, "variables"), index, value)
+        if (strcmp(json_string_value(json_object_get(variable, "name")), argv[1]) == 0)
         {
-          if (strcmp(json_string_value(json_object_get(value, "name")), argv[1]) == 0)
+          if (DEBUG)
+            printf("[DEBUG] Argument '%s' found in workspace.\n", argv[1]);
+          if (json_object_get(variable, "value") != NULL)
           {
-            json_t *variable = json_object_get(value, "value");
-            if (variable && (json_typeof(variable) == JSON_REAL || json_typeof(variable) == JSON_ARRAY))
+            if (DEBUG)
+              printf("[DEBUG] Argument '%s' value found in workspace.\n", argv[1]);
+            if (json_typeof(json_object_get(variable, "value")) == JSON_REAL)
             {
-              var_found = true;
-              if (json_typeof(variable) == JSON_REAL)
-                arg = json_number_value(variable);
-              else
+              freq[0] = json_number_value(json_object_get(variable, "value"));
+              if (DEBUG)
+                printf("[DEBUG] Value of '%s' is = %.6e.\n", argv[1], freq[0]);
+            }
+            else if (json_typeof(json_object_get(variable, "value")) == JSON_ARRAY)
+            {
+
+              // realloc(freq, json_array_size(json_object_get(variable, "value")));
+              if (DEBUG)
               {
-                // vector operation here
+                printf("[DEBUG] Argument '%s' is array with size of %d.\n", argv[1], SIZEOF_ARR(freq));
+                printf("[DEBUG] Argument '%s' is array with size of %d.\n", argv[1], sizeof(double));
+                printf("[DEBUG] Argument '%s' is array with size of %d.\n", argv[1], sizeofx(freq));
+                printf("[DEBUG] Argument '%s' is array with size of %d.\n", argv[1], sizeof freq);
+                printf("[DEBUG] Argument '%s' is array with size of %d.\n", argv[1], sizeof freq[0]);
               }
             }
-
-            break;
           }
+          else
+          {
+            if (DEBUG)
+              printf("[DEBUG] Argument not found in workspace.\n");
+          }
+          // if (variable && (json_typeof(variable) == JSON_REAL || json_typeof(variable) == JSON_ARRAY))
+          // {
+          //   var_found = true;
+          //   if (json_typeof(variable) == JSON_REAL)
+          //     *freq = json_number_value(variable);
+          //   else
+          //   {
+          //     // vector operation here
+          //   }
+          // }
+
+          break;
         }
+        if (DEBUG)
+          printf("[DEBUG] Argument not found in workspace.\n");
       }
-      json_decref(workspace);
     }
 
-    if (var_found)
-    {
-      printf("Variable found in workspace\n");
-    }
-    if (!var_found)
-      arg = atof(argv[1]);
+    // if (var_found)
+    // {
+    //   printf("Variable found in workspace\n");
+    // }
+    // if (!var_found)
+    //   *freq = atof(argv[1]);
   }
 
   /* calculate wavelength */
-  if (!args)
-  {
-    printf("%.6e\n", arg);
-    double wavelen = (double)(C0) / arg;
-    printf("%.6e", wavelen);
-  }
+  // if (SIZEOF_ARR(freq) == 1)
+  // {
+  //   printf("%.6e\n", freq[0]);
+  //   double wavelen = (double)(C0) / freq[0];
+  //   printf("%.6e", wavelen);
+  // }
 
   /* print out and add workspace */
-  if (stat("workspace.json", &stat_buff) == 0)
-  {
-    json_error_t *json_error = NULL;
-    json_t *workspace = json_load_file("workspace.json", 0, json_error);
-    if (workspace != NULL && json_typeof(workspace) == JSON_OBJECT)
-    {
-    }
-  }
-
+  // if (stat(WORKSPACE, &stat_buff) == 0)
+  // {
+  //   json_error_t *json_error = NULL;
+  //   json_t *workspace = json_load_file("workspace.json", 0, json_error);
+  //   if (workspace != NULL && json_typeof(workspace) == JSON_OBJECT)
+  //   {
+  //   }
+  // }
+  free(freq);
+  free(wlen);
+  json_decref(workspace);
   return 0;
-  
 }
---
+++ include/macros.h
@@ -0,0 +1,12 @@
+#define WORKSPACE "bashlab.json"
+
+
+/* constants */
+#define C0 299792458 // Speed of light in m/s
+
+
+#define SIZEOF_ARR(x) (sizeof(x) / sizeof((x)[0]))
+
+int sizeofx(void *arr){
+  return sizeof(arr);
+}
